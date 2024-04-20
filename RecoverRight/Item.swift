c1830369ca466f7d8a16c88086969ef716dada14
“acalderon20” <acalderon20@coe.edu>
2024-04-20 01:40:25 -0700

    Initial Commit

---
+++ RecoverRight/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  RecoverRight
+//
+//  Created by Adolfo Calderon on 4/20/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ RecoverRight/RecoverRightApp.swift
@@ -0,0 +1,32 @@
+//
+//  RecoverRightApp.swift
+//  RecoverRight
+//
+//  Created by Adolfo Calderon on 4/20/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct RecoverRightApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
